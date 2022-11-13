0778678abfc35e7e9761e62f07f11662563f0da5
Elisenda Marcual <98337931+ElisabetaNew@users.noreply.github.com>
2022-11-13 18:29:15 +0100

    Add files via upload

---
+++ src/src/main/java/cat/itacademy/barcelonactiva/MarcualMora/Elisenda/s05/t02/n01/model/repository/PartidaRepository.java
@@ -0,0 +1,15 @@
+package cat.itacademy.barcelonactiva.MarcualMora.Elisenda.s05.t02.n01.model.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import cat.itacademy.barcelonactiva.MarcualMora.Elisenda.s05.t02.n01.model.domain.Partida;
+
+@Repository
+public interface PartidaRepository extends JpaRepository <Partida, Integer> {
+		
+	//findAll(Sort sort)	 
+
+
+}
